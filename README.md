f52eaa78260f5b439dc6098fa0af19b1cb7571d5
araibear <gen-s@muh.biglobe.ne.jp>
2013-05-19 17:47:05 -0700

    Initial commit demo1

---
+++ README.md
@@ -0,0 +1,4 @@
+flaming-tyrion
+==============
+
+This repository is test application on Visual Basic 2010 Express.
---
+++ テストアプリ/テストアプリ/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Label1.Text = "こんにちは"
+    End Sub
+End Class
