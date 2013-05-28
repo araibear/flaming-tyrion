19c78176cd5922f9e27f383c128d673a9ab57cc0
Gen Sasaki <ge4n-s@muh.biglobe.ne.jp>
2013-05-28 17:24:54 +0900

    ログ出力とSQLサーバのサンプルコード作成

--- SQLserverへの接続/SQLserverへの接続/Form1.Designer.vb
+++ SQLserverへの接続/SQLserverへの接続/Form1.Designer.vb
@@ -23,8 +23,9 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Button1
@@ -36,29 +37,47 @@ Partial Class Form1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'DataGridView1
+        'TextBox1
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(21, 12)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.RowTemplate.Height = 21
-        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
-        Me.DataGridView1.TabIndex = 1
+        Me.TextBox1.Location = New System.Drawing.Point(53, 78)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(179, 19)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(53, 29)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 19)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(53, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 12)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "品番ID"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(292, 266)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 
 End Class
--- SQLserverへの接続/SQLserverへの接続/Form1.vb
+++ SQLserverへの接続/SQLserverへの接続/Form1.vb
@@ -2,39 +2,79 @@
 
     Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
 
-        '接続用変数の定義
-        'ADO.NETの仕組みを使います。
-        Dim strConnectSQL As String
-        Dim strSQL As String
-        Dim SQLDA As SqlClient.SqlDataAdapter
-        Dim SQLDS As New DataSet()
+        ' Windows認証のロジック  ここから*********************************************
+        ' 接続に必要な定数を設定=============================================
+        'Dim St As String
+        'Dim Cn As New System.Data.SqlClient.SqlConnection
+        'Dim SQL As System.Data.SqlClient.SqlCommand
+        'Dim ServerName As String = "192.168.5.64" 'サーバー名(またはIPアドレス)
+        'Dim DatabaseName As String = "Look.PPS" 'データベース
 
-        strConnectSQL = _
-          "Server=TEST;" & _
-          "Initial Catalog=test;" & _
-          "User ID=test_user;" & _
-          "Password=test_user;" & _
-          "Integrated Security=false"
+        'St = "Server=""192.168.5.64"";"
+        'St &= "integrated security=SSPI;"
+        'St &= "initial catalog = Look.PPS"
+        'Cn.ConnectionString = St
+        'SQL = Cn.CreateCommand
+        ' ここまではきめうちで設定しておく
 
-        'SQL構文を指定します。
-        strSQL = "select * from testTable"
+        ' SQLserver認証のロジック　　ここから*******************************************
 
+        ' 接続に必要な定数を設定=============================================
+        Dim St As String
+        Dim Cn As New System.Data.SqlClient.SqlConnection
+        Dim SQL As System.Data.SqlClient.SqlCommand
+        Dim ServerName As String = "192.168.5.64" 'サーバー名(またはIPアドレス)
+        Dim UserID As String = "looktest" 'ユーザーID
+        Dim Password As String = "looktest" 'パスワード
+        Dim DatabaseName As String = "Look.PPS" 'データベース
+        ' 定数終了==========================================================
+        ' 接続文字列を連結する
+        St = "Server=" & ServerName & ";"
+        St &= "User ID=" & UserID & ";"
+        St &= "Password=" & Password & ";"
+        St &= "Initial Catalog=" & DatabaseName
+        ' 接続文字列をConnectionStringにセットする
+        Cn.ConnectionString = St
+        SQL = Cn.CreateCommand
+        ' ここまではきめうちで設定しておく
+
+        ' SQLの値の部分を置き換えるメソッド　TextBox2に入力された数字で検索に行く
+        Dim creater As New SQLcreater
+        SQL.CommandText = creater.SearchHinban(TextBox2.Text.ToString)
+        'SQL.CommandText = "SELECT 品番 FROM 品番マスタ WHERE 品番ID = 1"
+        '　DBアクセスはプログラム以外でのエラー発生の可能性があるので、try句でくるむ。
         Try
-            '接続の設定を行います。
-            SQLDA = New SqlClient.SqlDataAdapter(strSQL, strConnectSQL)
+            Cn.Open()
 
-            'データセットに格納します。
-            SQLDA.Fill(SQLDS, "TEST")
-
-            'データグリッドビューのデータソースを設定
-            Me.DataGridView1.DataSource = SQLDS.Tables("TEST")
-
+            TextBox1.Text = SQL.ExecuteScalar
+            ' MsgBox(SQL.ExecuteScalar)
         Catch ex As Exception
-            MessageBox.Show(ex.Message)
+            '出力表示のTextBox1にエラーを出力　→余力があれば、log4netを用いてエラーログ出力に変えてみよう
+            TextBox1.Text = ex.ToString
         End Try
 
 
-
+        Cn.Close()
+        SQL.Dispose()
+        Cn.Dispose()
 
     End Sub
+    Public Class SQLcreater
+        ' SQL基本構文フィールド?には数字（品番ID）が入る
+        Dim preStr As String = "SELECT 品番 FROM 品番マスタ WHERE 品番ID = ?"
+
+        Public Function SearchHinban(ByRef strNumber As String)
+            Dim number As Integer
+            Try
+                number = Integer.Parse(strNumber)
+            Catch ex As Exception
+                number = 1
+            End Try
+            strNumber = number.ToString()
+            preStr = Replace(preStr, "?", strNumber)
+            Return preStr
+        End Function
+    End Class
+
+    
 End Class
