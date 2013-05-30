68482ac50caee4467ae3177631daac4bc398c729
Gen Sasaki <ge4n-s@muh.biglobe.ne.jp>
2013-05-30 18:10:48 +0900

    動的配列 tryparseなど

--- 配列/配列/Module1.vb
+++ 配列/配列/Module1.vb
@@ -1,4 +1,7 @@
 ﻿Module Module1
+    '数字でない文字列を入力した場合に変換する数値：デフォルト：0
+    Public ERRNUM As Integer = 1
+
 
     Sub Main()
         '文字列配列の宣言　変数名() As String
@@ -15,7 +18,7 @@
 
 
         ' １次元整数配列変数の初期化
-        Dim price() As Integer = {120, 80, 55, 300, 168}
+        Dim price() As Integer = {89, 105, 525, 480, 1980}
         ' 要素の値変更
         price(1) = price(1) * -1
         price(3) = price(3) * -1
@@ -31,12 +34,12 @@
         Dim cntStudent As Integer = 5
         ' 科目数
         Dim cntSubject As Integer = 3
-        ' 成績の2次元配列変数の初期化{国語, 算数, 理科}
-        Dim score(,) As Integer = {{75, 85, 90}, _
+        ' 成績の2次元配列変数の初期化{国語,数学, 英語}
+        Dim score(,) As Integer = {{75, 84, 90}, _
                                     {100, 80, 80}, _
-                                    {90, 90, 90}, _
+                                    {90, 89, 90}, _
                                     {65, 90, 80}, _
-                                    {80, 85, 100}}
+                                    {69, 85, 100}}
         ' 合計値の2次元配列変数の宣言
         Dim sum(0, 2) As Integer
         ' 平均値の2次元配列変数の宣言（データ型に注意）
@@ -55,7 +58,59 @@
         Next
 
 
-        '動的配列
-    End Sub
+        '動的配列ReDimで再定義可能
+        Dim aryDynamic() As Integer
+        Dim num As Integer
+        '入力した数の配列として再定義する
+        num = ParseInt_s(Console.ReadLine(), ERRNUM)
+        ReDim aryDynamic(num)
+        '配列は0から始まるため、-1する
+        For i As Integer = 0 To num - 1
+            aryDynamic(i) = ParseInt_s(Console.ReadLine(), ERRNUM)
+        Next
+        '実は、配列数外の値を表示しようとしてもこけない
+        For i As Integer = 0 To num
+            Console.WriteLine(aryDynamic(i).ToString)
+        Next
+        Console.WriteLine("最後には0が表示されましたか？")
 
+        '配列の代入とクローン
+
+        Dim ary1() As String = {"あ", "い", "う", "え", "お"}
+        Dim ary2() As String = {"か", "き", "く", "け", "こ"}
+        'まずは配列を見てみます。
+        For i As Integer = 0 To 4
+            Console.WriteLine("ary1(" & i & "):" & ary1(i))
+        Next
+        For i As Integer = 0 To 4
+            Console.WriteLine("ary2(" & i & "):" & ary2(i))
+        Next
+        '配列を代入
+        ary1 = ary2
+        '配列１の４つめに「せ」をセット
+        ary1(3) = "せ"
+        Console.WriteLine("ary1(3):" & ary1(3))
+        Console.WriteLine("ary2(3):" & ary2(3))
+        ary2 = {"か", "き", "く", "け", "こ"}
+        '配列の型だけ継承したい場合はcloneを使いましょう
+        ary1 = ary2.Clone
+        ary1(3) = "せ"
+        Console.WriteLine("ary1(3):" & ary1(3))
+        Console.WriteLine("ary2(3):" & ary2(3))
+    End Sub
+    '***************************************************************
+    ' 機能：入力文字列を安全に数字変換する関数
+    ' 第１引数：変換する文字列
+    ' 第２引数：エラーだった場合に置き換える数字
+    ' 戻値：置き換わった結果
+    '******1*********2*********3*********4*********5**********6*****
+    Public Function ParseInt_s(ByRef strNum As String, ByRef errNum As Integer)
+        Dim res As Integer
+        If (Integer.TryParse(strNum, res)) Then
+        Else
+            res = errNum
+            Console.WriteLine("********** ERROR :{0} 数字ではありません。 **********", strNum)
+        End If
+        Return res
+    End Function
 End Module
